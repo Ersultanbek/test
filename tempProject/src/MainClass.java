c18f3a7d59c1707278b6f21d7ad7387ee3864793
elgondin Ersultan <elgondin Ersultan>
2018-01-06 00:01:24 +0600

    v 0.1.1

--- tempProject/src/MainClass.java
+++ tempProject/src/MainClass.java
@@ -2,7 +2,7 @@
         * Created by Ersultanbek on 05.01.2018.
         */
 public class MainClass {
-
+    int i = 0;
     public static void main(String args[]){
          System.out.println("Hello World");
          System.out.println("ku ku bitch");
